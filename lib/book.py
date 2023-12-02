1295219f9c9f953a16295d379dc1793f734a1cb1
TradingCyborg <my.github.kinyuad29@gmail.com>
2023-12-02 15:17:59 +0300

    book

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        assert isinstance(page_count, int), "page_count must be an integer"
+        self.title = title
+        self.page_count = page_count
+
+# Example usage:
+if __name__ == "__main__":
+    # Creating instances of the Book class
+    book1 = Book("The Great Gatsby", 180)
+    book2 = Book("To Kill a Mockingbird", 281)
+
+    # Accessing and printing attributes
+    print(book1.title)  # Output: The Great Gatsby
+    print(book1.page_count)  # Output: 180
+
+    print(book2.title)  # Output: To Kill a Mockingbird
+    print(book2.page_count)  # Output: 281
+
+    # Creating a Book instance with a non-integer page_count (will raise an AssertionError)
+    try:
+        invalid_book = Book("Invalid Book", "Not an Integer")
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, model, size, color):
+        self.brand = brand
+        self.model = model
+        self.size = size
+        self.color = color
+        self.is_worn = False  # Assume the shoe is not worn initially
+
+    def wear(self):
+        if not self.is_worn:
+            print(f"You are now wearing the {self.color} {self.brand} {self.model}.")
+            self.is_worn = True
+        else:
+            print("The shoes are already worn.")
+
+    def take_off(self):
+        if self.is_worn:
+            print("You have taken off the shoes.")
+            self.is_worn = False
+        else:
+            print("The shoes are not currently worn.")
+
+# Example usage:
+shoe1 = Shoe("Nike", "Air Max", 10, "Blue")
+shoe2 = Shoe("Adidas", "Superstar", 9, "White")
+
+print(shoe1.brand)  # Output: Nike
+print(shoe2.size)  # Output: 9
+
+shoe1.wear()  # Output: You are now wearing the Blue Nike Air Max.
+shoe1.take_off()  # Output: You have taken off the shoes.
+
+shoe2.wear()  # Output: You are now wearing the White Adidas Superstar.
+shoe2.wear()  # Output: The shoes are already worn.
+
+    
